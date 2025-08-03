7b10fccb5a0ffe56a55b71c081a2ce2fab445966
unknown <mayur7024k@gmail.com>
2025-08-03 12:37:25 +0530

    Landing page and Navbar implementation

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,21 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "CV_SCAN" },
+    { name: "description", content: "Analyze yourself for dream jobs!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your Resume and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>
 }
